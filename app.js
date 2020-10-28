b771fed932fa3d4bad0bcecde8df60a7de0071ff
chinmay <catrawalkar@gmail.com>
2020-10-28 13:28:17 +0530

    'post-to-facebook'

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-let Parser = require('rss-parser');
-let parser = new Parser();
+require("dotenv").config();
+const Parser = require("rss-parser");
+const parser = new Parser();
+const fs = require("fs");
+const request = require("request");
 
-(async () => {
+const parseSite = async (site) => {
+  let feed = await parser.parseURL(site.rssfeed);
 
-  let feed = await parser.parseURL('https://www.newsbharati.com/rssfeed.rss');
-  console.log(feed.title);
+  fs.readFile(site.log, "utf8", function (err, file) {
+    if (err) return console.log(err);
 
-  feed.items.forEach(item => {
-    console.log(item.title + ':' + item.link)
- });
-})();
+    feed.items.forEach((item) => {
+      if (!file.includes(item.link)) {
+        const data = item.title + ":" + item.link + "\n";
+        fs.appendFile(site.log, data, function (err) {
+          if (err) return console.log(err);
+        });
+
+        request.post(
+          `https://graph.facebook.com/v8.0/2039216499697073/feed?link=${item.link}&message=${item.description}&access_token=${process.env.TOKEN}`
+        );
+        return;
+      }
+      console.log("already there");
+    });
+  });
+};
+
+const sites = [
+  {
+    rssfeed: "https://www.newsbharati.com/rssfeed.rss",
+    title: "newsbharati",
+    log: "log.newsbharati.txt",
+  },
+];
+
+sites.map((site) => {
+  parseSite(site);
+});
